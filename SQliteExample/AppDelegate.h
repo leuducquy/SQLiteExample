d0ec25abf5b3e5a8609d61ef3f439978d5422138
Quy LD <quy-ld@trente.asia>
2016-12-11 19:31:37 +0700

    Initial Commit

---
+++ SQliteExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQliteExample
+//
+//  Created by quy on 12/11/16.
+//  Copyright © 2016 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQliteExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQliteExample
+//
+//  Created by quy on 12/11/16.
+//  Copyright © 2016 quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
